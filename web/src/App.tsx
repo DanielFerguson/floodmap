fb72bbc10fa38a2d89ed9025764ac1cb409f3e9a
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-10-14 17:13:34 +1100

    added serverless offline and typescript to api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import Map, { GeolocateControl, NavigationControl, Source, Layer } from 'react-map-gl';
 import { PencilIcon, UserCircleIcon } from '@heroicons/react/24/outline'
 
@@ -17,12 +18,28 @@ const layerStyle = {
   }
 };
 
+type Props = {
+  children?: ReactNode;
+};
+
+const Button = ({ children }: Props) => {
+  return (
+    <button
+      type="button"
+      disabled
+      className="inline-flex items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+    >
+      {children}
+    </button>
+  )
+}
+
 function App() {
   return (
     <div>
       <main className='relative'>
-        {/* Toolbar */}
-        <nav className='absolute z-20 top-0 left-0 bg-white m-6 px-6 py-4 rounded-lg shadow flex gap-12'>
+        {/* Desktop Toolbar */}
+        <nav className='hidden sm:flex absolute z-20 top-0 left-0 bg-white m-6 px-6 py-4 rounded-lg shadow flex gap-12'>
           {/* Branding */}
           <div className='flex items-center gap-3'>
             <img src="/favicon.svg" alt="Flood map icon" className='h-6 w-auto' />
@@ -32,25 +49,40 @@ function App() {
           {/* Actions */}
           <div className='flex gap-3'>
             {/* Add Road */}
-            <button
-              type="button"
-              disabled
-              className="inline-flex items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            >
+            <Button>
               <PencilIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
               Add Road
-            </button>
+            </Button>
             {/* Login/Logout */}
-            <button
-              type="button"
-              className="inline-flex items-center rounded border border-gray-300 bg-white px-2.5 py-1.5 text-xs font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            >
+            <Button>
               <UserCircleIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
               Login
-            </button>
+            </Button>
           </div>
         </nav>
 
+        {/* Mobile Toolbar */}
+        <nav className='sm:hidden z-20 flex justify-between absolute bottom-0 right-0 left-0 mx-6 mb-10 rounded px-6 py-4 bg-white shadow-lg'>
+          {/* Branding */}
+          <div className='flex items-center gap-3'>
+            <img src="/favicon.svg" alt="Flood map icon" className='h-6 w-auto' />
+            <h1 className='font-permanent'>Flood Map</h1>
+          </div>
+
+          {/* Actions */}
+          <div className='flex gap-3'>
+            <Button>
+              <PencilIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+              Add Road
+            </Button>
+            <Button>
+              <UserCircleIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+              Login
+            </Button>
+          </div>
+        </nav>
+
+        {/* Map */}
         <Map
           initialViewState={{
             longitude: 143.8503,
