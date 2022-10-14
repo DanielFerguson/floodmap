9095c89473ae7e478ff905444a2a30bdf81ae212
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-10-15 00:05:00 +1100

    Updated app import

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app'
+import App from './App'
 import './index.css'
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { Auth0Provider } from "@auth0/auth0-react";
