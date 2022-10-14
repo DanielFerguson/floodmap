407c832cf8435dad1c7668d84df7cd0ab25f9e78
Daniel Ferguson <thedanielfergusonkid@gmail.com>
2022-10-14 15:11:18 +1100

    setup mapbox to show full page map

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
 function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
 
   return (
     <div>
-      {/* TODO: Header */}
-
-      <div>
-        <main>
-          <h1 className="text-3xl font-bold">Hello, world!</h1>
-        </main>
-      </div>
+      <main>
+        <div ref={mapContainer} className="h-screen w-screen" />
+      </main>
     </div>
   )
 }
